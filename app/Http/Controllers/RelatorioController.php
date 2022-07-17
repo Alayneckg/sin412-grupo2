34f3c4bf783929b4497445050875b1099c28369c
Alayne <alayneckg@hotmail.com>
2022-07-17 14:46:41 -0300

    Bug and fixes

--- app/Http/Controllers/RelatorioController.php
+++ app/Http/Controllers/RelatorioController.php
@@ -21,30 +21,32 @@ class RelatorioController extends Controller
     {
         $tarefas = Auth::user()->tarefas()->get();
         $relatorio = [];
-        $relatorio['criadas'] = 0;
-        $relatorio['interrompidas'] = 0;
-        $relatorio['finalizadas'] = 0;
-        $relatorio['total'] = count($tarefas);
-        $relatorio['total_ciclos'] = 0;
-        $relatorio['total_foco'] = 0;
-        $relatorio['frequencia'] = [];
-        foreach($tarefas as $tarefa){
-            if($tarefa->status == 'To do'){
-                $relatorio['criadas']++;
+        if(isset($tarefas)){
+            $relatorio['criadas'] = 0;
+            $relatorio['interrompidas'] = 0;
+            $relatorio['finalizadas'] = 0;
+            $relatorio['total'] = count($tarefas);
+            $relatorio['total_ciclos'] = 0;
+            $relatorio['total_foco'] = 0;
+            $relatorio['frequencia'] = [];
+            foreach($tarefas as $tarefa){
+                if($tarefa->status == 'To do'){
+                    $relatorio['criadas']++;
+                }
+                if($tarefa->status == 'Doing'){
+                    $relatorio['interrompidas']++;
+                }
+                if($tarefa->status == 'Done'){
+                    $relatorio['finalizadas']++;
+                    $relatorio['total_foco'] = $relatorio['total_foco'] + $tarefa->ciclo->tempo_foco;
+                }
+                $relatorio['total_ciclos'] = $relatorio['total_ciclos'] + $tarefa['qtd_ciclos'];
+                $criacao = (new Datetime($tarefa->created_at))->format('Y-m-d');
+                $relatorio['frequencia'][$criacao][] = $tarefa;
             }
-            if($tarefa->status == 'Doing'){
-                $relatorio['interrompidas']++;
-            }
-            if($tarefa->status == 'Done'){
-                $relatorio['finalizadas']++;
-                $relatorio['total_foco'] = $relatorio['total_foco'] + $tarefa->ciclo->tempo_foco;
-            }
-            $relatorio['total_ciclos'] = $relatorio['total_ciclos'] + $tarefa['qtd_ciclos'];
-            $criacao = (new Datetime($tarefa->created_at))->format('Y-m-d');
-            $relatorio['frequencia'][$criacao][] = $tarefa;
+            $relatorio['media_ciclos'] = $relatorio['total_ciclos']/$relatorio['total'];
+            $relatorio['total_dias'] = count($relatorio['frequencia']);
         }
-        $relatorio['media_ciclos'] = $relatorio['total_ciclos']/$relatorio['total'];
-        $relatorio['total_dias'] = count($relatorio['frequencia']);
         return view('analytics',[
             'tarefas' => $tarefas,
             'relatorio' => $relatorio,
@@ -56,42 +58,44 @@ class RelatorioController extends Controller
         $tarefas = Auth::user()->tarefas()->get();
         $usuarios = User::get();
         $relatorio = [];
-        $relatorio['tdah'] = count(User::where('tdah',true)->get());
-        $relatorio['usuarios'] = count($usuarios);
-        $relatorio['criadas'] = 0;
-        $relatorio['interrompidas'] = 0;
-        $relatorio['finalizadas'] = 0;
-        $relatorio['tarefas'] = count($tarefas);
-        $relatorio['total_ciclos'] = 0;
-        $relatorio['total_foco'] = 0;
-        $relatorio['frequencia'] = [];
-        foreach($tarefas as $tarefa){
-            $criacao = (new Datetime($tarefa->created_at))->format('Y-m-d');
-            $relatorio['frequencia'][$criacao]['Todo'] = [];
-            $relatorio['frequencia'][$criacao]['Doing'] = [];
-            $relatorio['frequencia'][$criacao]['Done'] = [];
-            if($tarefa->status == 'To do'){
-                $relatorio['criadas']++;
-                $relatorio['frequencia'][$criacao]['Todo'][] = '';
+        if(isset($tarefas)){
+            $relatorio['tdah'] = count(User::where('tdah',true)->get());
+            $relatorio['usuarios'] = count($usuarios);
+            $relatorio['criadas'] = 0;
+            $relatorio['interrompidas'] = 0;
+            $relatorio['finalizadas'] = 0;
+            $relatorio['tarefas'] = count($tarefas);
+            $relatorio['total_ciclos'] = 0;
+            $relatorio['total_foco'] = 0;
+            $relatorio['frequencia'] = [];
+            foreach($tarefas as $tarefa){
+                $criacao = (new Datetime($tarefa->created_at))->format('Y-m-d');
+                $relatorio['frequencia'][$criacao]['Todo'] = [];
+                $relatorio['frequencia'][$criacao]['Doing'] = [];
+                $relatorio['frequencia'][$criacao]['Done'] = [];
+                if($tarefa->status == 'To do'){
+                    $relatorio['criadas']++;
+                    $relatorio['frequencia'][$criacao]['Todo'][] = '';
+                }
+                if($tarefa->status == 'Doing'){
+                    $relatorio['interrompidas']++;
+                    $relatorio['frequencia'][$criacao]['Doing'][] = '';
+                }
+                if($tarefa->status == 'Done'){
+                    $relatorio['finalizadas']++;
+                    $relatorio['frequencia'][$criacao]['Done'][] = '';
+                    $relatorio['total_foco'] = $relatorio['total_foco'] + $tarefa->ciclo->tempo_foco;
+                }
+                $relatorio['total_ciclos'] = $relatorio['total_ciclos'] + $tarefa['qtd_ciclos'];
+                $relatorio['frequencia'][$criacao]['total'][] = $tarefa;
             }
-            if($tarefa->status == 'Doing'){
-                $relatorio['interrompidas']++;
-                $relatorio['frequencia'][$criacao]['Doing'][] = '';
+            $relatorio['media_ciclos'] = $relatorio['total_ciclos']/$relatorio['tarefas'];
+            $relatorio['total_dias'] = count($relatorio['frequencia']);
+            foreach($relatorio['frequencia'] as $dado){
+                $relatorio['tempo'] = $tarefa->ciclo->tempo_foco + $tarefa->ciclo->tempo_pausa;
             }
-            if($tarefa->status == 'Done'){
-                $relatorio['finalizadas']++;
-                $relatorio['frequencia'][$criacao]['Done'][] = '';
-                $relatorio['total_foco'] = $relatorio['total_foco'] + $tarefa->ciclo->tempo_foco;
-            }
-            $relatorio['total_ciclos'] = $relatorio['total_ciclos'] + $tarefa['qtd_ciclos'];
-            $relatorio['frequencia'][$criacao]['total'][] = $tarefa;
+            $relatorio['media_tempo'] = $relatorio['tempo'] / count($relatorio['frequencia']);
         }
-        $relatorio['media_ciclos'] = $relatorio['total_ciclos']/$relatorio['tarefas'];
-        $relatorio['total_dias'] = count($relatorio['frequencia']);
-        foreach($relatorio['frequencia'] as $dado){
-            $relatorio['tempo'] = $tarefa->ciclo->tempo_foco + $tarefa->ciclo->tempo_pausa;
-        }
-        $relatorio['media_tempo'] = $relatorio['tempo'] / count($relatorio['frequencia']);
 
         return view('analytics_admin',[
             'tarefas' => $tarefas,
